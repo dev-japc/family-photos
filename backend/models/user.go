e007605e68ae0dacdeb29c7a6da99be9b02dee10
dev-japc <devjapc@proton.me>
2026-06-04 22:41:50 -0400

    feat: add jwt-go dependency, update core packages, and refactor user models with password masking

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,22 +3,23 @@ package models
 import (
 	"time"
 )
-//white list for family members allowence
+
+// AllowedNationalIdNumber representa la lista blanca de familiares autorizados.
 type AllowedNationalIdNumber struct {
-	ID uint `gorm:"primaryKey" json:"id"`
-	NationalIdNumber string `gorm: "unique;not null;type:varchar(20)" json:"national_id_number"`
-	Name string `gorm:"not null" json:"name"`
-	CreatedAt time.Time `json:"created_at""`
+	ID               uint      `gorm:"primaryKey" json:"id"`
+	NationalIdNumber string    `gorm:"unique;not null;type:varchar(20)" json:"national_id_number"`
+	Name             string    `gorm:"not null" json:"name"`
+	CreatedAt        time.Time `json:"created_at"`
 }
 
-//users
+// User representa a los usuarios registrados.
 type User struct {
-	ID uint `gorm:"primaryKey" json:"id"`
-	FullName string `gorm:"not null" json:"full_name"`
-	Email string `gorm:"unique; not null" json:"email"`
-	NationalIdNumber string `gorm:"unique; not null;type:varchar(20)" json:"national_id_number"`
-	Password string `gorm:"not null" json:"password"`
-	Role string `gorm:"default:'family_member'; not null" json:"role"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID               uint      `gorm:"primaryKey" json:"id"`
+	FullName         string    `gorm:"not null" json:"full_name"`
+	Email            string    `gorm:"unique;not null" json:"email"`
+	NationalIdNumber string    `gorm:"unique;not null;type:varchar(20)" json:"national_id_number"`
+	Password         string    `gorm:"not null" json:"-"`
+	Role             string    `gorm:"default:'family_member';not null" json:"role"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
